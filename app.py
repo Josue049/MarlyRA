5911b889945c9175ea69d7f261cc8e64056963d7
Josue049 <josue09guti@gmail.com>
2025-09-29 21:20:30 -0500

    static no model endpoint

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/model')
-def model():
-    # Sirve el modelo desde la carpeta static
-    return send_from_directory('static', 'bracelet.glb')
-
 if __name__ == "__main__":
     # host="0.0.0.0" => visible en la red local
     app.run(host="0.0.0.0", port=5000, debug=True)
